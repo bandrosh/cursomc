0eb67bd9fcf1f9d43ca5c2ffb3a53a2a1bf71dad
Leonardo Jales <leonardo@larces.uece.br>
2018-05-11 14:24:13 -0300

    ItemPedido e ItemPedidoPK entidades criadas

--- src/main/java/com/leonardo/cursomc/domain/Pedido.java
+++ src/main/java/com/leonardo/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.leonardo.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,6 +35,9 @@ public class Pedido implements Serializable{
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> items = new HashSet<>();
+	
 	public Pedido() {
 		super();
 	}
@@ -84,6 +90,14 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public Set<ItemPedido> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<ItemPedido> items) {
+		this.items = items;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -108,6 +122,5 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
-
 	
 }
